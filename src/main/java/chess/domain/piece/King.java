1a4ffd755c26cd3f6716ee27f095722112a4aa9b
Zeus6768 <akaps6768@gmail.com>
2024-03-19 16:55:45 +0900

    feat: 모든 체스 기물을 생성한다.

--- src/main/java/chess/domain/piece/King.java
+++ src/main/java/chess/domain/piece/King.java
@@ -1,40 +1,17 @@
 package chess.domain.piece;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import static chess.domain.attribute.File.E;
 
-import chess.domain.chessboard.Chessboard;
-import chess.domain.chessboard.attribute.Direction;
-import chess.domain.piece.attribute.Color;
-import chess.domain.piece.attribute.Movement;
-import chess.domain.piece.attribute.Position;
+import chess.domain.attribute.Color;
+import chess.domain.attribute.File;
+import chess.domain.attribute.Position;
+import chess.domain.attribute.Rank;
 
 public class King extends UnslidingPiece {
 
-    private static final Position WHITE_INITIAL_POSITION = Position.from("e1");
-    private static final Position BLACK_INITIAL_POSITION = Position.from("e8");
+    private static final File START_FILE = E;
 
-    private static final Set<Movement> POSSIBLE_MOVEMENTS = Direction.all()
-            .stream()
-            .map(Movement::of)
-            .collect(Collectors.toUnmodifiableSet());
-
-    public King(final Color color, final Position position) {
-        super(color, position);
-    }
-
-    public static King ofInitialPosition(final Color color) {
-        return new King(color, selectByColor(color, WHITE_INITIAL_POSITION, BLACK_INITIAL_POSITION));
-    }
-
-    @Override
-    public Piece move(final Chessboard chessboard, final Position target) {
-        validateTarget(movablePositions(chessboard), target);
-        return new King(color(), target);
-    }
-
-    @Override
-    public Set<Position> movablePositions(final Chessboard chessboard) {
-        return movablePositions(chessboard, POSSIBLE_MOVEMENTS);
+    public King(final Color color) {
+        super(color, Position.of(START_FILE, Rank.startRankOf(color)));
     }
 }
