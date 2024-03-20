faedd91c37d2e1f585f685f1b24149ebf4c228da
Zeus6768 <akaps6768@gmail.com>
2024-03-20 17:50:36 +0900

    feat(Square): 현재위치에서 방향을 입력받아 다음 위치를 반환한다

--- src/main/java/chess/domain/chessboard/attribute/Direction.java
+++ src/main/java/chess/domain/chessboard/attribute/Direction.java
@@ -1,16 +1,12 @@
 package chess.domain.chessboard.attribute;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public enum Direction {
-    UP(-1, 0),
-    UP_LEFT(-1, -1),
-    UP_RIGHT(-1, 1),
-    DOWN(1, 0),
-    DOWN_LEFT(1, -1),
-    DOWN_RIGHT(1, 1),
+    UP(1, 0),
+    UP_LEFT(1, -1),
+    UP_RIGHT(1, 1),
+    DOWN(-1, 0),
+    DOWN_LEFT(-1, -1),
+    DOWN_RIGHT(-1, 1),
     LEFT(0, -1),
     RIGHT(0, 1)
     ;
@@ -23,24 +19,11 @@ public enum Direction {
         this.column = column;
     }
 
-    public static Set<Direction> all() {
-        return Arrays.stream(values())
-                .collect(Collectors.toUnmodifiableSet());
-    }
-
-    public static Set<Direction> orthogonal() {
-        return Set.of(UP, RIGHT, DOWN, LEFT);
-    }
-
-    public static Set<Direction> diagonal() {
-        return Set.of(UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT);
-    }
-
-    public int row() {
+    public int getRow() {
         return row;
     }
 
-    public int column() {
+    public int getColumn() {
         return column;
     }
 }
