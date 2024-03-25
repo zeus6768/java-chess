fa7ae8c236be49c2832cc4dcd132b4691392824d
Zeus6768 <akaps6768@gmail.com>
2024-03-25 09:28:53 +0900

    refactor: 체스판 출력 로직 개선

--- src/main/java/chess/view/dto/SquareCharacter.java
+++ src/main/java/chess/view/dto/SquareCharacter.java
@@ -14,45 +14,43 @@ import chess.domain.piece.attribute.Color;
 
 public enum SquareCharacter {
 
-    KING(King.class, "k", "K"),
-    QUEEN(Queen.class, "q", "Q"),
-    BISHOP(Bishop.class, "b", "B"),
-    KNIGHT(Knight.class, "n", "N"),
-    ROOK(Rook.class, "r", "R"),
-    PAWN(AbstractPawn.class, "p", "P");
+	KING(King.class, "k", "K"),
+	QUEEN(Queen.class, "q", "Q"),
+	BISHOP(Bishop.class, "b", "B"),
+	KNIGHT(Knight.class, "n", "N"),
+	ROOK(Rook.class, "r", "R"),
+	PAWN(AbstractPawn.class, "p", "P");
 
-    private static final String EMPTY = ".";
+	private final Class<? extends Piece> pieceClass;
+	private final String white;
+	private final String black;
 
-    private final Class<? extends Piece> pieceClass;
-    private final String white;
-    private final String black;
+	<P extends Piece> SquareCharacter(final Class<P> pieceClass, final String white, final String black) {
+		this.pieceClass = pieceClass;
+		this.white = white;
+		this.black = black;
+	}
 
-    <P extends Piece> SquareCharacter(final Class<P> pieceClass, final String white, final String black) {
-        this.pieceClass = pieceClass;
-        this.white = white;
-        this.black = black;
-    }
+	public static String from(final Square square) {
+		if (square.isEmpty()) {
+			return ".";
+		}
+		Piece piece = square.piece();
+		return Arrays.stream(values())
+				.filter(pieceType -> pieceType.isAssignableFrom(piece.getClass()))
+				.map(character -> character.valueBy(piece.color()))
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new);
+	}
 
-    public static String from(final Square square) {
-        if (square.isEmpty()) {
-            return EMPTY;
-        }
-        Piece piece = square.piece();
-        return Arrays.stream(values())
-                .filter(pieceType -> pieceType.isAssignableFrom(piece.getClass()))
-                .map(character -> character.valueBy(piece.color()))
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
-    }
+	private <P extends Piece> boolean isAssignableFrom(final Class<P> other) {
+		return pieceClass.isAssignableFrom(other);
+	}
 
-    private <P extends Piece> boolean isAssignableFrom(final Class<P> other) {
-        return pieceClass.isAssignableFrom(other);
-    }
-
-    private String valueBy(final Color color) {
-        if (color.isBlack()) {
-            return black;
-        }
-        return white;
-    }
+	private String valueBy(final Color color) {
+		if (color.isBlack()) {
+			return black;
+		}
+		return white;
+	}
 }
