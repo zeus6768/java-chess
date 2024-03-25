0e2b58bd8249f2761f96747488ce5d84843c9532
Zeus6768 <akaps6768@gmail.com>
2024-03-26 01:40:53 +0900

    refactor: 체스판 생성 로직 분리

--- src/main/java/chess/domain/chessboard/ChessboardFactory.java
+++ src/main/java/chess/domain/chessboard/ChessboardFactory.java
@@ -1,12 +1,14 @@
 package chess.domain.chessboard;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import chess.domain.chessboard.attribute.File;
 import chess.domain.chessboard.attribute.Rank;
-import chess.domain.chessboard.attribute.Squares;
+import chess.domain.chessboard.attribute.Square;
 import chess.domain.piece.Bishop;
 import chess.domain.piece.King;
 import chess.domain.piece.Knight;
@@ -23,38 +25,50 @@ public class ChessboardFactory {
     }
 
     public static Chessboard empty() {
-        return new Chessboard(emptyBoard());
+        return new Chessboard(emptySquares());
     }
 
     public static Chessboard create() {
-        List<Squares> chessboard = emptyBoard();
+        List<List<Square>> chessboard = emptySquares();
         putInitialPieces(chessboard);
         return new Chessboard(chessboard);
     }
 
     public static Chessboard of(final Piece... pieces) {
-        List<Squares> chessboard = emptyBoard();
+        List<List<Square>> chessboard = emptySquares();
         for (final Piece piece : pieces) {
             Position position = piece.position();
             Rank rank = position.rank();
-            Squares squares = chessboard.get(rank.toRow());
-            squares.put(position, piece);
+            File file = position.file();
+            List<Square> row = chessboard.get(rank.toRow());
+            row.set(file.toColumn(), Square.from(piece));
         }
         return new Chessboard(chessboard);
     }
 
-    private static List<Squares> emptyBoard() {
-        return Arrays.stream(Rank.values())
-                .map(__ -> Squares.empty())
-                .toList();
+    private static List<List<Square>> emptySquares() {
+        List<List<Square>> chessboard = new ArrayList<>();
+        for (final Rank ignored : Rank.values()) {
+            initialize(chessboard);
+        }
+        return Collections.unmodifiableList(chessboard);
     }
 
-    private static void putInitialPieces(final List<Squares> chessboard) {
+    private static void initialize(final List<List<Square>> chessboard) {
+        List<Square> squares = new ArrayList<>();
+        for (final File ignored : File.values()) {
+            squares.add(Square.empty());
+        }
+        chessboard.add(squares);
+    }
+
+    private static void putInitialPieces(final List<List<Square>> chessboard) {
         for (final Piece piece : initialPieces()) {
             Position position = piece.position();
             Rank rank = position.rank();
-            Squares squares = chessboard.get(rank.toRow());
-            squares.put(position, piece);
+            File file = position.file();
+            List<Square> squares = chessboard.get(rank.toRow());
+            squares.set(file.toColumn(), Square.from(piece));
         }
     }
 
