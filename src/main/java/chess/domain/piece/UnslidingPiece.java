81f12879baf70868eacdcbd9a4b8084c0b7517cb
Zeus6768 <akaps6768@gmail.com>
2024-03-29 16:35:08 +0900

    refactor: 반복문 -> stream 변경 등

--- src/main/java/chess/domain/piece/UnslidingPiece.java
+++ src/main/java/chess/domain/piece/UnslidingPiece.java
@@ -1,11 +1,10 @@
 package chess.domain.piece;
 
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import chess.domain.chessboard.Chessboard;
-import chess.domain.chessboard.attribute.Square;
 import chess.domain.piece.attribute.Color;
 import chess.domain.piece.attribute.Movement;
 import chess.domain.piece.attribute.Position;
@@ -17,23 +16,18 @@ public abstract class UnslidingPiece extends Piece {
     }
 
     protected Set<Position> movablePositions(final Chessboard chessboard, final Set<Movement> movements) {
-        Set<Position> positions = new HashSet<>();
-        for (final Movement movement : movements) {
-            movablePosition(chessboard, movement).ifPresent(positions::add);
-        }
-        return positions;
+        return movements.stream()
+                .map(movement -> movablePosition(chessboard, movement))
+                .flatMap(Optional::stream)
+                .collect(Collectors.toUnmodifiableSet());
     }
 
     private Optional<Position> movablePosition(final Chessboard chessboard, final Movement movement) {
-        Optional<Position> possiblePosition = position().after(movement);
-        if (possiblePosition.isEmpty()) {
-            return Optional.empty();
-        }
-        Position position = possiblePosition.get();
-        Square square = chessboard.squareIn(position);
-        if (square.isEmpty() || isAttackable(square)) {
-            return Optional.of(position);
-        }
-        return Optional.empty();
+        return position().after(movement)
+                .filter(position -> isMovable(chessboard, position));
+    }
+
+    private boolean isMovable(final Chessboard chessboard, final Position position) {
+        return chessboard.isEmpty(position) || isAttackable(chessboard, position);
     }
 }
