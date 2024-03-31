afb5a8b389379e060a5b04367ba46eef79e6bdf8
Zeus6768 <akaps6768@gmail.com>
2024-03-31 18:20:29 +0900

    feat: chessboard <-> fen 변환 기능 구현

---
+++ src/main/java/chess/fen/Fen.java
@@ -0,0 +1,69 @@
+package chess.fen;
+
+import static chess.fen.PieceFenMapper.fenFrom;
+import static chess.fen.PieceFenMapper.pieceOf;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import chess.domain.chessboard.Chessboard;
+import chess.domain.chessboard.ChessboardFactory;
+import chess.domain.chessboard.attribute.Rank;
+import chess.domain.piece.Piece;
+
+public class Fen {
+
+    private static final String PATTERN = "^(?:[rnbqkpRNBQKP1-8]{1,8}/){7}[rnbqkpRNBQKP1-8]{1,8}$";
+
+    private final String fen;
+
+    public Fen(final String fen) {
+        if (!fen.matches(PATTERN)) {
+            throw new IllegalArgumentException("FEN 형식에 맞지 않습니다: %s".formatted(fen));
+        }
+        this.fen = fen;
+    }
+
+    public static Fen from(final Chessboard chessboard) {
+        StringBuilder fen = new StringBuilder();
+        for (final Rank rank : Rank.values()) {
+            List<Piece> pieces = chessboard.getAllFrom(rank);
+            appendFens(pieces, fen);
+            fen.append('/');
+        }
+        return new Fen(fen.toString());
+    }
+
+    private static void appendFens(final List<Piece> pieces, final StringBuilder fen) {
+        for (final Piece piece : pieces) {
+            fen.append(fenFrom(piece));
+        }
+    }
+
+    public Chessboard toChessboard() {
+        Set<Piece> pieces = new HashSet<>();
+        String[] piecesByFen = fen.split("/");
+        for (int row = 0; row < piecesByFen.length; row++) {
+            pieces.addAll(toPieces(piecesByFen[row], row));
+        }
+        return ChessboardFactory.from(pieces);
+    }
+
+    private Set<Piece> toPieces(final String rowByFen, final int row) {
+        Set<Piece> pieces = new HashSet<>();
+        int column = 0;
+        for (final char squareByFen : rowByFen.toCharArray()) {
+            pieceOf(squareByFen, row, column).ifPresent(pieces::add);
+            column += getIfDigit(squareByFen);
+        }
+        return pieces;
+    }
+
+    private int getIfDigit(final char fen) {
+        if (Character.isDigit(fen)) {
+            return Character.getNumericValue(fen);
+        }
+        return 1;
+    }
+}
